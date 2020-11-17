2ec29bd43c45e5335349a6a7d7e6addb51481886
Antonio Canongo <canongo11@gmail.com>
2020-11-16 22:42:15 -0800

    Unit test

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,116 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("Not a palindrome");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("prrp");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("rare");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("dog_and_cats");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("miim");
+	ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("Sunday");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("FRIday");
+	ASSERT_FALSE(actual);
+}
+
+
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("rare");
+	ASSERT_FALSE(actual);
+}
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("rain");
+	EXPECT_FALSE(false );
+}
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("rare");
+	ASSERT_FALSE(false);
+}
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("rare");
+	EXPECT_TRUE(true)
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("oooo");
+	EXPECT_TRUE(true)
+}
+
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("computer_science");
+	EXPECT_FALSE(false)
+}
+
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("chico_state");
+	EXPECT_FALSE(false)
+}
+
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("lalal");
+	EXPECT_FALSE(false)
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("okko");
+	EXPECT_TRUE(true)
+}
+
+TEST(PracticeTest, symbols_not_palindrome)
+{
+	PracticeTest obj;
+	bool actual = obj.isPalindrome("sceince");
+	EXPECT_FALSE(false)
+}
